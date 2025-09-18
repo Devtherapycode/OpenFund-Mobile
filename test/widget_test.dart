ff7baafbc25ade9edbb9f482ddd066392635f95e
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-09-19 02:05:39 +0400

    Initial Flutter project setup with platforms support

---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Basic app test', (WidgetTester tester) async {
+    // TODO: Add widget tests when app structure is finalized
+    expect(true, isTrue);
+  });
+}
